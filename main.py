2091d3b96665335fdf5edcdfe982c9cb80195b6e
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-09 13:59:05 +0300

    Starting up on my scraping codes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
 print('Kashee list the skills you are good at: ')
 familiarSkills = input('> ')
-jobHunt = familiarSkills.split()
+jobHunt = familiarSkills.split(", ")
+print(f"Filtering Out {familiarSkills} ")
+
+def findJob():
+    __finder = requests.get('https://www.linkedin.com/jobs/')
+    print(__finder)
